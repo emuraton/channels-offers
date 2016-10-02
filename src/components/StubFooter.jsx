6ad287f87838772d5240cc39e115f83da1d6777c
Emilien <emilien_muraton@yahoo.fr>
2016-10-02 16:46:29 +0200

    Some stub react component to organize spa.

---
+++ src/components/StubFooter.jsx
@@ -0,0 +1,22 @@
+import React, {Component, PropTypes} from 'react';
+
+class StubFooter extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    // const {myBeautifulProps} = this.props;
+
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+StubFooter.propTypes = {
+  //Some props
+}
+
+export default StubFooter;
---
+++ src/components/StubNavBar.jsx
@@ -0,0 +1,22 @@
+import React, {Component, PropTypes} from 'react';
+
+class StubNavBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    // const {myBeautifulProps} = this.props;
+
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+StubNavBar.propTypes = {
+  //Some props
+}
+
+export default StubNavBar;
